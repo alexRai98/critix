919a4f1fdea21b2213f40c198aca53b17b679c21
Rai <raikcuerda19@gmail.com>
2020-05-19 22:08:21 -0500

    add column title and add references game to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_025104) do
+ActiveRecord::Schema.define(version: 2020_05_20_030608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_025104) do
     t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["game_id"], name: "index_critics_on_game_id"
   end
 
@@ -36,7 +37,10 @@ ActiveRecord::Schema.define(version: 2020_05_20_025104) do
     t.string "genre"
     t.integer "price"
     t.date "release_date"
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_games_on_company_id"
   end
 
   add_foreign_key "critics", "games"
+  add_foreign_key "games", "companies"
 end
