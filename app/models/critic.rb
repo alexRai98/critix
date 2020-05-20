a85770c18d56cd02807da746e464b4c4b646602e
Rai <raikcuerda19@gmail.com>
2020-05-20 00:46:51 -0500

    add validation critic

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,7 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :username, presence: true, length: { in: 2..12 }, format: { with: /\A[a-zA-Z\d]+\z/,
+    message: "only allows letters and numbers" }
+  validates :body, presence: true,exclusion: {in: [nil],message:"Please write something!"}
+  validates :title, presence: true, exclusion: {in: [nil],message:"Please write something!"}
 end
