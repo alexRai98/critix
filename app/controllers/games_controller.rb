7af88e4e41d682ce405b19c197dc258d16ccc076
Rai <raikcuerda19@gmail.com>
2020-05-20 21:30:37 -0500

    games create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,27 @@
 class GamesController < ApplicationController
+
     def index
         @games = Game.all
         render json:@games 
     end
+
     def show
         @games= Game.find(params[:id])
         render json:@games
     end
+
+    def create
+        @game = Game.new(game_params)
+        if @game.save
+            render json: @game
+        else
+            render json: @game.errors
+        end
+    end
+    
+    private
+    def game_params
+        params.require(:game).permit(:name, :genre, :price, :release_date, :company_id)
+    end
+
 end
