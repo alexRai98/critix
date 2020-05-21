6a641be031c3bf1bc6ab062ca2a2e7fa322880bb
Rai <raikcuerda19@gmail.com>
2020-05-20 21:55:29 -0500

    update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,4 +24,19 @@ class GamesController < ApplicationController
         params.require(:game).permit(:name, :genre, :price, :release_date, :company_id)
     end
 
+    def destroy
+        @game = Game.find(params[:id]) # Find the game you want to destroy.
+        @game.destroy # Destroy it.
+        head :no_content # Render nothing back
+    end
+        
+    def update
+        @game = Game.find(params[:id]) #Find the game you want to update
+        if @game.update_attributes(game_params) # Validation using the same private method as before
+        render json: @game # render the updated record back
+        else
+        render json: @game.errors, status: :unprocessable_entity # render errors
+        end
+    end
+
 end
